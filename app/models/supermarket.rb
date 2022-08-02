e013c4e3d55b16125468dfed049ba39e2112e6f2
Ethan Nguyen <etnguyen16@gmail.com>
2022-08-02 14:34:40 -0600

    add top three and instock method

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,3 +1,11 @@
 class Supermarket < ApplicationRecord
   has_many :customers
+
+  def top_three
+    customers.joins(:items).select('items.*, COUNT(items.*) AS count').group('items.id').order('count desc').limit(3)
+  end 
+
+  def instock_items 
+    customers.joins(:items).select('items.*').distinct
+  end 
 end
